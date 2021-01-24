77d45c5c1c839d50bd5f1513852464247849ce16
Noah Day <sheltielover432@gmail.com>
2021-01-24 17:48:42 -0500

    Create TestDatabase.sql

---
+++ TestDatabase.sql
@@ -0,0 +1,27 @@
+create database test;
+use test;
+create table addresses
+(
+	id INT,
+    house_number INT,
+    city VARCHAR(20),
+    zipcode varchar(7)
+);
+
+create table people
+(
+	id int,
+    first_name varchar(20),
+    last_name varchar(20),
+    address_id INT
+);
+
+create table pets
+(
+	id int,
+    name VARCHAR(20),
+    species VARCHAR(20),
+    owner_id INT
+);
+
+show tables;
