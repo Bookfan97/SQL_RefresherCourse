5b5bf5a5528d19f736c5b742c91965df3946b2a8
Noah Day <sheltielover432@gmail.com>
2021-01-24 20:40:23 -0500

    Excercises

--- TestDatabase.sql
+++ TestDatabase.sql
@@ -59,3 +59,34 @@ alter table pets change `species` `animal_type` VARCHAR(20);
 alter table addresses modify city varchar(30);
 
 alter table addresses modify city char(30);
+
+
+/*Exercise 1*/
+
+/*1. Add a primary key to the id fields in the pets and people tables.*/
+alter table pets 
+add primary key (id);
+
+
+alter table people 
+add primary key (id);
+
+/*2. Add a foreign key to the owner_id field in the pets table referencing the id field in the people table. */
+
+alter table pets 
+add constraint FK_ownerID
+foreign key (owner_id) REFERENCES people(id); 
+
+/*3. Add a column named email to the people table. */
+ALTER TABLE people
+ADD COLUMN email VARCHAR(20);
+
+/*4. Add a unique constraint to the email column in the people table.*/
+alter table people
+add constraint u_email unique (email);
+
+/*5. Rename the name column in the pets table to ‘first_name’.*/
+alter table pets change `name` `first_name` VARCHAR(20);
+
+/*6. Change the postcode data type to CHAR(7) in the addresses table.*/
+ALTER TABLE addresses MODIFY zipcode CHAR(7);
