0ac92c60df36896c13edff9d472d23285d947496
Noah Day <sheltielover432@gmail.com>
2021-01-24 21:48:34 -0500

    Update DeleteExample.sql

--- DeleteExample.sql
+++ DeleteExample.sql
@@ -8,4 +8,16 @@ create table people(
     gender enum('M', 'F')
 );
 
+insert into people (name,age,gender)
+values
+('Emma', 21, 'F'),
+('John', 30, 'M'),
+('Thomas', 27, 'M'),
+('Chris', 44, 'M'),
+('Sally', 23, 'F'),
+('Frank', 55, 'M');
+
 select * from people;
+
+delete from people 
+where name = 'John';
